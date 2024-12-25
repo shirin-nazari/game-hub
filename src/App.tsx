dcd8adbaa359ed304d263cc902021e28b8c2d0f1
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2024-12-25 17:05:22 +0330

    build color mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
-import { Button } from '@chakra-ui/react';
+import { Grid, GridItem, Show } from '@chakra-ui/react';
+import NavBar from './components/NavBar';
+
 function App() {
-  return <Button colorScheme="blue">Click me</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav">
+        <NavBar />
+      </GridItem>
+
+      <Show above="lg">
+        <GridItem area="aside">Aside</GridItem>
+      </Show>
+      <GridItem area="main">Main</GridItem>
+    </Grid>
+  );
 }
+
 export default App;
