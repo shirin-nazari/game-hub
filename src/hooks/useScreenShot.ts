c6805b7aa26912309c54f58a80cfc7bed4cd4d0b
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2025-01-05 19:42:19 +0330

    finish

--- src/hooks/useScreenShot.ts
+++ src/hooks/useScreenShot.ts
@@ -1,6 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import APIClient from '@/services/api-client';
-import Screenshot from '@/entities/screenshot';
+import Screenshot from '@/entities/Screenshots';
 
 const useScreenShot = (gameId: number) => {
   const apiClient = new APIClient<Screenshot>(`/games/${gameId}/screenshots`);
